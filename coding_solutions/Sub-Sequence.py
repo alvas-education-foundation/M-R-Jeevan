4abcbb72aa36ff20e508eb89a70397ead825f5a9
M R JEEVAN <mrjeevan2000@gmail.com>
2020-05-20 23:09:14 +0530

    Sub-Sequence

---
+++ coding_solutions/Sub-Sequence.py
@@ -0,0 +1,20 @@
+  
+'''
+"A subsequence is a sequence that can be derived from another sequence by deleting some elements 
+without changing the order of the remaining elements"
+
+Q.A user will input two strings, and we find if one of the strings is a sub sequence of the other. 
+Program prints “yes” if either the first string is a sub sequence of the second string or the 
+second string is a sub sequence of the first string.
+
+Assume that, the length of the first string is smaller than or equal to the length of the second string.
+
+Expected output :
+
+Input the first string : tree
+Input the second string : Computer science is awesome
+YES
+'''
+
+
+
